750038b24b4b1ef30edef5deb10e3c44cdd38b0c
huantd <tranhuan251000@gmail.com>
2021-07-21 13:53:11 +0700

    update dialog

--- src/components/Form/Create.vue
+++ src/components/Form/Create.vue
@@ -164,7 +164,7 @@ export default {
         maintenance: [
           {
             required: true,
-            message: "Yêu cầu nhập ngày kết thúc (dự kiến)",
+            message: "Yêu cầu nhập ngày kết thúc",
             trigger: "blur",
           },
         ],
